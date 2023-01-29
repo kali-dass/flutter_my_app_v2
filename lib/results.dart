28c9ab6e9962f054c907a7db1c48e4c7e2051868
kali-dass <kalidass@gmail.com>
2023-01-29 01:31:35 +0000

    using getter function and if else + removed const constructor from test script

--- lib/results.dart
+++ lib/results.dart
@@ -7,11 +7,23 @@ class Result extends StatelessWidget {
 
   Result(this.resultScore);
 
+  String get resultPhrase {
+    var resultText = '';
+    if (resultScore > 15) {
+      resultText = 'That is awesome.';
+    } else if (resultScore > 10) {
+      resultText = 'That is cool. ';
+    } else {
+      resultText = 'Can make better choice. ';
+    }
+    return resultText + "Your score is " + resultScore.toString();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        'Well done',
+        resultPhrase,
         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
       ),
     );
